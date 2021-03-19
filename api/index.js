46b2af7aabd9fe1b0904df8365ac2715e3bf2150
Lezurex <lenny.angst@easyid.ch>
2021-03-19 12:21:41 +0100

    Add rate limiting

--- api/index.js
+++ api/index.js
@@ -2,12 +2,24 @@ const express = require("express");
 const path = require("path");
 const Database = require("./database/Database");
 const routes = require('./routes');
+const RateLimit = require('express-rate-limit');
+const config = require('./../config/config.json');
 const app = express(),
     port = 3080;
+
+const limiter = new RateLimit({
+    windowMs: config.rateLimit.timeWindow * 60 * 1000,
+    max: config.rateLimit.maxRequests,
+    message: JSON.stringify({
+        code: 429,
+        error: "You sent too many requests! Limit is set to " + config.rateLimit.maxRequests + " requests in " + config.rateLimit.timeWindow + " minutes."
+    })
+})
 
 Database.connect();
 
 app.use(express.static(path.join(__dirname, '../webapp/dist')));
+app.use(limiter);
 
 // Bind all api routes to the endpoint
 app.use('/api', routes);
