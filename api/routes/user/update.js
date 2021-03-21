0bd1ddb4375310b5215ad860d62fe5902cbc2bae
Lezurex <lenny.angst@easyid.ch>
2021-03-21 18:03:58 +0100

    Add update to user CRUD

--- api/routes/user/update.js
+++ api/routes/user/update.js
@@ -1,6 +1,21 @@
-module.exports = (req, res) => {
-    if (req.session.user.isAdmin || req.session.user.id === req.user.id) {
+const bcrypt = require('bcrypt');
 
+module.exports = async (req, res) => {
+    if (req.session.user.isAdmin || req.session.user.id === req.user.id) {
+        let data = req.body;
+        let user = req.user;
+        data.email ? user.email = data.email : null;
+        data.firstname ? user.firstname = data.firstname : null;
+        data.lastname ? user.lastname = data.lastname : null;
+        if (req.session.user.isAdmin) {
+            data.isAdmin ? user.isAdmin = data.isAdmin : null;
+        }
+        if (data.password) {
+            let hash = await bcrypt.hash(data.password, 10);
+            user.password = hash;
+        }
+        user.saveToDB();
+        res.status(200).json(user.toJSON());
     } else {
         res.status(403).json({
             error: "Forbidden",
