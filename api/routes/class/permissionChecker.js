4bdf8ff1e062b730870d61e398a0f4a945c363f3
Lezurex <lenny.angst@easyid.ch>
2021-03-24 17:11:28 +0100

    Add comments to permissionChecker.js

--- api/routes/class/permissionChecker.js
+++ api/routes/class/permissionChecker.js
@@ -5,11 +5,24 @@
  * Last modified: 22.03.21, 13:12
  */
 
+/**
+ * Checks if a user has the specified permission in the specified group.
+ * @param user {User} The user to be checked for
+ * @param classObj {Class} The class where the user has to have the permission
+ * @param permissionName {String} The permissions name (see {@link Role.permissions})
+ * @returns {boolean} Whether or not the user has the permission.
+ */
 function hasPermission(user, classObj, permissionName) {
     let relation = Object.values(user.classRelations).find(relation => relation.class.id === classObj.id && relation.role.permissions[permissionName]);
     return !!relation;
 }
 
+/**
+ * Checks if a user is in the specified class.
+ * @param user {User} The user to be checked for
+ * @param classObj {Class} The class where the user has to be in
+ * @returns {boolean} Whether or not the user is in the class.
+ */
 function isInClass(user, classObj) {
     let relation = Object.values(user.classRelations).find(relation => relation.class.id === classObj.id);
     return !!relation;
