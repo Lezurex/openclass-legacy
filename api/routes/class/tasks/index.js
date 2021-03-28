33413045d781c28ef931e3cb13dfb3f9444ef3df
Lezurex <lenny.angst@easyid.ch>
2021-03-28 14:33:48 +0200

    Add single task getter

--- api/routes/class/tasks/index.js
+++ api/routes/class/tasks/index.js
@@ -2,12 +2,21 @@
  * Copyright (c) 2021 Lenny Angst. All rights reserved.
  * For more information about the license read the LICENSE file at the root of this repo.
  * Written for Project: openclass
- * Last modified: 22.03.21, 07:49
+ * Last modified: 28.03.21, 14:31
  */
 
 const tasks = require('express').Router({mergeParams: true});
 const all = require('./all');
 const single = require('./single');
+
+tasks.param('taskId', ((req, res, next, value) => {
+    const task = Object.values(req.class.tasks).find(task => task.id + '' === value);
+    if (task) {
+        req.task = task;
+        next();
+    } else
+        res.status(404).json({ error: "Requested task not found in class!", code: 404 })
+}))
 
 tasks.get('/', all);
 tasks.get('/:taskId', single);
--- api/routes/class/tasks/single.js
+++ api/routes/class/tasks/single.js
@@ -2,17 +2,12 @@
  * Copyright (c) 2021 Lenny Angst. All rights reserved.
  * For more information about the license read the LICENSE file at the root of this repo.
  * Written for Project: openclass
- * Last modified: 22.03.21, 07:49
+ * Last modified: 28.03.21, 14:32
  */
 
 module.exports = (req, res) => {
-    let task = Object.values(req.class.tasks).find(task => task.id === (req.params.taskId * 1));
-    if (task) {
-        res.status(200).json(task);
-    } else {
-        res.status(404).json({
-            error: "Task not found in class!",
-            code: 404
-        })
-    }
+    let task = req.task.toJSON();
+    // Adding subject details
+    task.subject = global.subjects[task.subject];
+    res.status(200).json(task);
 }
