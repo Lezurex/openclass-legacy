480653aa24b233b6818e194d81457c60a93efb19
Lezurex <lenny.angst@easyid.ch>
2021-03-30 18:07:03 +0200

    Begin sidebar

--- webapp/src/App.vue
+++ webapp/src/App.vue
@@ -2,13 +2,13 @@
   - Copyright (c) 2021 Lenny Angst. All rights reserved.
   - For more information about the license read the LICENSE file at the root of this repo.
   - Written for Project: openclass
-  - Last modified: 3/30/21, 3:42 PM
+  - Last modified: 3/30/21, 4:07 PM
   -->
 
 <template>
   <div class="flex">
     <navbar v-if="global.API.auth.loggedIn"></navbar>
-    <router-view class="flex-grow" />
+    <router-view class="flex-grow ml-20 p-4" />
   </div>
 </template>
 
--- webapp/src/components/Navbar.vue
+++ webapp/src/components/Navbar.vue
@@ -2,15 +2,25 @@
   - Copyright (c) 2021 Lenny Angst. All rights reserved.
   - For more information about the license read the LICENSE file at the root of this repo.
   - Written for Project: openclass
-  - Last modified: 3/30/21, 3:44 PM
+  - Last modified: 3/30/21, 6:06 PM
   -->
 
 <template>
   <aside>
-    <nav>
-      <ul>
-        <li>
-          <router-link to="/tasks"></router-link>
+    <nav
+        class="fixed bg-gray-300 dark:bg-gray-800 transition-all sm:-bottom-0 sm:w-full sm:h-20 md:top-0 md:w-20 md:h-full sidenavbar">
+      <ul class="list-none p-0 m-0 flex flex-nowrap items-center h-full sm:flex-row">
+        <li class="sidenav-item">
+          <router-link class="sidenav-link" to="/tasks">
+            <span class="link-text">{{ $t('pages.tasks') }}</span>
+            <i class="fas fa-book"></i>
+          </router-link>
+        </li>
+        <li class="sidenav-item">
+          <router-link class="sidenav-link" to="/classes">
+            <span class="link-text">{{ $t('pages.tasks') }}</span>
+            <i class="fas fa-book"></i>
+          </router-link>
         </li>
       </ul>
     </nav>
@@ -24,5 +34,50 @@ export default {
 </script>
 
 <style scoped>
+
+.sidenav-item {
+  @apply w-full
+}
+
+.sidenav-item:last-child {
+  @apply mt-auto
+}
+
+.sidenav-link {
+  @apply
+  flex
+  items-center
+  h-20
+  bg-gray-300
+  dark:bg-gray-800
+  no-underline
+  transition
+  w-full
+  sm:justify-center;
+  filter: grayscale(100%) opacity(.7);
+}
+
+.sidenav-link:hover, .sidenav-link.router-link-active {
+  @apply bg-gray-200 dark:bg-gray-700;
+  filter: grayscale(0%) opacity(1);
+}
+
+.link-text {
+  @apply hidden ml-4;
+}
+
+@media only screen and (min-width: 640px) {
+  .sidenavbar {
+    flex-direction: column!important;
+  }
+
+  .sidenavbar:hover {
+    @apply w-64;
+  }
+
+  .sidenavbar:hover .link-text {
+    @apply inline transition-opacity;
+  }
+}
 
 </style>
